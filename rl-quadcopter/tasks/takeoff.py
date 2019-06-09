f6c50de554e271ddc6521a7335dab163d8fd18d1
osullivanp <osullivanp@dnb.com>
2019-06-09 16:04:13 +0100

    Adding all the files for my udacity quadcopter project

---
+++ rl-quadcopter/tasks/takeoff.py
@@ -0,0 +1,91 @@
+import numpy as np
+from physics_sim import PhysicsSim
+
+class Task():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., target_pos=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+
+        # Goal
+        if target_pos is None :
+            print("Setting default init pose")
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        #reward = np.tanh(1 - 0.003*(abs(self.sim.pose[:3] - self.target_pos))).sum()
+        #reward = 1 - 0.003*(abs(self.sim.pose[:3] - self.target_pos))).sum()
+        #distance_from_target = abs(self.sim.pose[:3] - self.target_pos
+        #reward = np.tanh(1 - 0.003*(distance_from_target))).sum()
+        
+        x = sum(abs(self.sim.pose[:3] - np.float32(self.target_pos))) / 3
+        #print(x)
+        distance_from_target = self.sigmoid(x)
+        reward = 1.0 - distance_from_target
+        done = False
+        
+        if self.sim.pose[2] >= self.target_pos[2]: # agent has crossed the target height
+            reward += 30.0  # bonus reward (set value as per your expeience on the project)
+            done = True
+            
+        return reward, done
+        #return reward
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            #delta_reward, done_height = self.get_reward()
+            
+            # penalize crash
+            if done and self.sim.time < self.sim.runtime:
+                #print("Crash")
+                reward = -10
+            else:
+                delta_reward, done_height = self.get_reward()
+            
+                if done_height:
+                    done = done_height
+                    reward += delta_reward
+            
+            #reward += self.get_reward()
+            pose_all.append(self.sim.pose)
+            
+            # penalize crash
+            #if done:
+            #    if self.sim.time < self.sim.runtime:
+            #        reward = -10
+            #    else:
+            #        reward += 10
+            #if done :
+            #    reward += 10
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
