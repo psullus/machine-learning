a1122ecf605f54ead262ba08d2b0c692af258395
osullivanp <osullivanp@dnb.com>
2019-07-21 15:21:45 +0100

    text clean up

--- capstone/models/baseline_model.py
+++ capstone/models/baseline_model.py
@@ -18,19 +18,22 @@ class BaselineModel():
         self.predictions = None
 
     def splitData(self, p=False):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.data['text'], self.data['labels'], random_state=1)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.data['Text'], self.data['Labels'], random_state=1)
 
         if p:
+            print(' ')
             print('Number of rows in the total set: {}'.format(self.data.shape[0]))
             print('Number of rows in the training set: {}'.format(self.X_train.shape[0]))
             print('Number of rows in the test set: {}'.format(self.X_test.shape[0]))
             print(' ')
             print(self.data.head())
 
-    def fitNaiveBayes(self):
+    def cvDataset(self):
         self.count_vector = CountVectorizer()
         self.training_data = self.count_vector.fit_transform(self.X_train)
         self.testing_data = self.count_vector.transform(self.X_test)
+
+    def fitNaiveBayes(self):
         self.naive_bayes = MultinomialNB()
         self.naive_bayes.fit(self.training_data, self.y_train)
 
@@ -38,6 +41,7 @@ class BaselineModel():
         self.predictions = self.naive_bayes.predict(self.testing_data)
 
     def printScores(self, posLabel=4):
+        print(' ')
         print('Accuracy score: ', format(accuracy_score(self.y_test, self.predictions)))
         print('Precision score: ', format(precision_score(self.y_test, self.predictions, pos_label=posLabel)))
         print('Recall score: ', format(recall_score(self.y_test, self.predictions, pos_label=posLabel)))
